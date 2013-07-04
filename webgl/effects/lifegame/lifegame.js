be31975d5d800c5fefa720570c0a0938c39d40dd
Seiei Higa <hanachin@gmail.com>
2013-07-05 01:07:36 +0900

    LifeGameをクラスにした

--- webgl/effects/lifegame/lifegame.js
+++ webgl/effects/lifegame/lifegame.js
@@ -1,10 +1,9 @@
-var LifeGame = {}
-LifeGame.load = function(){
+var LifeGame = function(){
   this.size = 512;
-  this.calcShader = new ShaderObject({vert: 'vertex.vert', frag: 'calc.frag'});
-  this.noiseShader = new ShaderObject({vert: 'vertex.vert', frag: 'noise.frag'});
-  this.renderShader = new ShaderObject({vert: 'vertex.vert', frag: 'render.frag'});
-  this.messageShader = new ShaderObject({vert: 'image.vert', frag: 'image.frag'});
+  this.calcShader    = new ShaderObject({vert: 'vertex.vert', frag: 'calc.frag'});
+  this.noiseShader   = new ShaderObject({vert: 'vertex.vert', frag: 'noise.frag'});
+  this.renderShader  = new ShaderObject({vert: 'vertex.vert', frag: 'render.frag'});
+  this.messageShader = new ShaderObject({vert: 'image.vert',  frag: 'image.frag'});
   this.messageTextures = [
     new TextureObject({size:128,image:createCharImage('い',128)}),
     new TextureObject({size:128,image:createCharImage('い',128)}),
@@ -14,7 +13,7 @@ LifeGame.load = function(){
   var texture1 = new TextureObject({size: this.size, filter:GL.NEAREST});
   this.target0 = new RenderTarget({texture: texture0})
   this.target1 = new RenderTarget({texture: texture1})
-  this.quad = new ArrayBufferObject(2, [-1, -1, 1, -1, 1, 1, -1, 1]);
+  this.quad    = new ArrayBufferObject(2, [-1, -1, 1, -1, 1, 1, -1, 1]);
 
    // GL.framebuffer.setRenderTarget(this.target0);
    // this.noiseShader.use({
@@ -22,7 +21,7 @@ LifeGame.load = function(){
    // }).render(GL.TRIANGLE_FAN, 4, {vertex: this.quad});
 }
 var a=0;
-LifeGame.render = function(target){
+LifeGame.prototype.render = function(target){
   GL.framebuffer.setRenderTarget(this.target1);
   this.calcShader.use({
     dx: [1 / this.size, 0],
@@ -44,7 +43,6 @@ LifeGame.render = function(target){
     rect: [-1+0.65*2,1-(a*0.013)%3,0.7,1],
     texture: this.messageTextures[2]
   }).render(GL.TRIANGLE_FAN, 4, {vertex: this.quad});
-  
 }
 
   var target_tmp = this.target0;
